a76ca50c2b28373783d9affe520e45f8efed8603
inmin617 <144991175+inmin617@users.noreply.github.com>
2023-10-12 16:16:32 +0900

    실습3

--- main.c
+++ main.c
@@ -3,16 +3,55 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void func1(void) {
-	int x;
-	printf("func1 x is at %p\n", &x); // &는 메모리의 주소 의미 
+int sumTwo(int a, int b) 
+{
+	int sum=a+b;
+	return sum;
 	
 }
+
+int square(int n)
+{
+	int squared=n*n;
+	return squared;
+
+}
+
+int get_max(int x, int y)
+{
+	int max;
 	
-int main(void){
-	int x;
-	printf("main x is at %p\n", &x);
-	func1();
+	if (x<y)
+		max=y;
+	if (x>y)
+		max=x;
+	else 
+		max=x=y;
+		
+	return max;
+}
+
+int main(int argc, char *argv()){
 	
+	int a, b;
+	a=4;
+	b=7;
+	sumTwo(4, 7);
+	printf("sumTwo result will be %i\n", sumTwo(4, 7));
+	
+	square(4);
+	printf("square result will be %i\n", square(4));
+	
+	int x, y;
+	x=10;
+	y=6;
+	
+	get_max(10, 6);
+	printf("two integers will be %d and %d, so the max value will be %d.", x, y, get_max(10,6));
+	
+	system("PAUSE");
 	return 0;
+
 }
+
+
