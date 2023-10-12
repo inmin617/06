743fc18d92a4bbf33e597f530210200578eb869d
inmin617 <144991175+inmin617@users.noreply.github.com>
2023-10-12 16:26:14 +0900

    실습4

--- main.c
+++ main.c
@@ -3,55 +3,21 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo(int a, int b) 
+
+
+int square (int a)
 {
-	int sum=a+b;
-	return sum;
-	
-}
-
-int square(int n)
+	return(a*a);
+} 
+ 
+int main()
 {
-	int squared=n*n;
-	return squared;
-
-}
-
-int get_max(int x, int y)
-{
-	int max;
-	
-	if (x<y)
-		max=y;
-	if (x>y)
-		max=x;
-	else 
-		max=x=y;
-		
-	return max;
-}
-
-int main(int argc, char *argv()){
-	
-	int a, b;
-	a=4;
-	b=7;
-	sumTwo(4, 7);
-	printf("sumTwo result will be %i\n", sumTwo(4, 7));
-	
-	square(4);
-	printf("square result will be %i\n", square(4));
-	
-	int x, y;
-	x=10;
-	y=6;
-	
-	get_max(10, 6);
-	printf("two integers will be %d and %d, so the max value will be %d.", x, y, get_max(10,6));
+	int a=2;
+	a=square(a);
+	printf("a=%i\n", a);
 	
 	system("PAUSE");
 	return 0;
-
 }
 
 
